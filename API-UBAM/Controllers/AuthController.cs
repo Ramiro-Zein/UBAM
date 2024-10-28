96e192049570e26c189fa727b22070a074985293
Ramiro-Zein <ramirozeincontrerasrangel@gmail.com>
2024-10-28 16:18:57 -0600

    Primera parte Login

---
+++ API-UBAM/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using API_UBAM.DatabaseContext;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_UBAM.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly UbamDbContext _context;
+
+    public AuthController(UbamDbContext context)
+    {
+        _context = context;
+    }
+    
+}
---
+++ API-UBAM/Models/LoginRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace API_UBAM.Models;
+
+public class LoginRequest
+{
+    public string Nombre_Usuario { get; set; }
+    public string Clave_Usuario { get; set; }
+}
