e19242da2b0f6245ea4bf3d63da8d03e5a563414
Ramiro Zein <75920457+Ramiro-Zein@users.noreply.github.com>
2024-10-28 14:45:52 -0600

    ControllerAlumnos

---
+++ WEB-UBAM/Controllers/AlumnosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_UBAM.Controllers;
+
+public class AlumnosController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
